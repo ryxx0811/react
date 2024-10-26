5766e5a34d92ca06556cab21fdde1c501ea90fad
ryxx0811 <yaxi.liu.0811@gmail.com>
2024-10-26 16:01:26 +0200

    pro_4

---
+++ react/react_04/src/components/AuthInputs.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import {styled} from 'styled-components';
+import CustomInput from './Input';
+import Button from './Button';
+
+const ControlContainer=styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`
+
+export default function AuthInputs() {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleInputChange(identifier, value) {
+    if (identifier === 'email') {
+      setEnteredEmail(value);
+    } else {
+      setEnteredPassword(value);
+    }
+  }
+
+  function handleLogin() {
+    setSubmitted(true);
+  }
+
+  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const passwordNotValid = submitted && enteredPassword.trim().length < 6;
+
+  return (
+    <div id="auth-inputs">
+      <ControlContainer>
+          <CustomInput
+            label='Email'
+            type="email"
+            $invalid={emailNotValid}
+            onChange={(event) => handleInputChange('email', event.target.value)}
+          />
+
+          <CustomInput
+            label='Password'
+            type="password"
+            $invalid={passwordNotValid}
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+          />
+        
+      </ControlContainer>
+      <div className="actions">
+        <button type="button" className="text-button">
+          Create a new account
+        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
+      </div>
+    </div>
+  );
+}
