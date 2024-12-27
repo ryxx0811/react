edb7ef14ca868782bbb1fa336c1ec34b3afc04d8
ryxx0811 <yaxi.liu.0811@gmail.com>
2024-12-27 16:04:56 +0100

    react 07

---
+++ react/react_07/src/App.jsx
@@ -0,0 +1,121 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import SelectedProject from "./components/SelectedProject";
+
+import {useState} from 'react';
+
+function App() {
+  const [projectsState,setProjectsState]=useState({
+    selectedProjectId:undefined,
+    projects:[],
+    tasks:[]
+  });
+
+  
+
+  function handleSelectedProject(id){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:id,
+      };
+    });
+  }
+
+  function handleStartAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:null,
+      };
+    });
+  }
+
+  function handleCancelAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+      };
+    });
+  }
+
+  function hanleAddProject(projectData){
+    setProjectsState(prevState=>{
+      const projectId=Math.random();
+      const newProject={
+        ...projectData,
+        id:projectId,
+      };
+        
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:[...prevState.projects,newProject]
+      }
+    });
+  }
+
+  function handleDeletProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+        projects:prevState.projects.filter((project)=>project.id!==prevState.selectedProjectId)
+      };
+    });
+  }
+
+  function handleAddTask(text){
+    setProjectsState(prevState=>{
+      const taskId=Math.random();
+      const newTask={
+        text:text,
+        projectId:prevState.selectedProjectId,
+        id:taskId,
+      };
+        
+      return{
+        ...prevState,
+        tasks:[newTask,...prevState.tasks]
+      }
+    });
+  }
+
+  function handleDeleteTask(id){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        tasks:prevState.tasks.filter((task)=>task.id!==id),
+      };
+    });
+  }
+
+  const selectedProject=projectsState.projects.find(project=>project.id===projectsState.selectedProjectId);
+
+  let content=<SelectedProject project={selectedProject} 
+                               onDelete={handleDeletProject} 
+                               onAddTask={handleAddTask}
+                               onDeleteTask={handleDeleteTask}
+                               tasks={projectsState.tasks}/>;
+
+  if(projectsState.selectedProjectId===null){
+    content=<NewProject onAdd={hanleAddProject} onCancel={handleCancelAddProject}/>
+  }else if (projectsState.selectedProjectId===undefined){
+    content=<NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  };
+  
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar 
+        onStartAddProject={handleStartAddProject} 
+        projects={projectsState.projects}
+        onSelectProject={handleSelectedProject}
+        selectedProjectId={projectsState.selectedProjectId}/>
+      {content} 
+    </main>
+  );
+}
+
+export default App;
